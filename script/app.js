1a51cadaa3312a27785938382a50a09ea4adab60
Eric Lin <tinyjingle@gmail.com>
2014-05-11 20:08:47 -0400

    First page done

--- script/app.js
+++ script/app.js
@@ -14,12 +14,37 @@ $(document).ready(function(){
 	//Window setting
 	var height = $(window).height() - 80;
 	var width = $(window).width();
-	$(".slide-1").css("height", (height));
-	$(".viewport-container").css("height", ($(document).height()));
+	var scrolled = false;
 
-	//first page
-	$(".rectangle").css("left", ((1 - 50/width) * 50 + "%"));
-	$(".triangle").css("left", ((1 - 100/width) * 50 + "%"));
+	var init = function() {
+		$(".slide-1").css("height", (height));
+		if (height < 500) {
+			$(".scroll-container").hide();
+		}
+	}
+
+
+	var first = $("div#first");
+	var runAnima = function() {
+  		first.delay(200).fadeTo(600, 0.3);
+  		first.next().delay(400).fadeTo(600, 0.3);
+  		first.next().next().delay(600).fadeTo(600, 0.3);
+  		first.next().next().next().delay(800).fadeTo(600, 0.3);
+  		first.delay(600).fadeTo(600, 1);
+  		first.next().delay(700).fadeTo(600, 1);
+  		first.next().next().delay(800).fadeTo(600, 1);
+  		first.next().next().next().delay(900).fadeTo(600, 1);
+  		if(!scrolled) {
+  			setTimeout(runAnima, 4000);
+  		}
+	}
+
+	init();
+	runAnima();
+
+    $(window).scroll(function() {
+        scrolled = true;
+    })
 
   	$(".logo").click(function() {
   		console.log("height is " + height);

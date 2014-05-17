2b864ffd8e45f5cafbb378a320701b08a54ef32d
Eric Lin <tinyjingle@gmail.com>
2014-05-17 15:33:32 -0400

    Second page text and style

--- script/app.js
+++ script/app.js
@@ -10,46 +10,44 @@
 
 $(document).ready(function(){
 
-	//Window setting
-	var height = $(window).height() - 80;
-	var width = $(window).width();
-	var scrolled = false;
+    //Window setting
+    var height = $(window).height() - 80;
+    var width = $(window).width();
+    var scrolled = false;
 
-	var init = function() {
-		$(".slide-1").css("height", (height));
-		if (height < 500) {
-			$(".scroll-container").hide();
-		}
+    var init = function() {
+        $(".slide-1").css("height", (height));
+        if (height < 500) {
+            $(".scroll-container").hide();
+        }
 
-		$(".menu").attr(("data-" + height), "background-color:rgba(192, 57, 43, 1); color: rgb(255, 255, 255);");
-		$(".menu").attr(("data-" + (height + 1)), "background-color:rgba(255, 255, 255, 0.9); color: rgb(150, 40, 27)");
-		var s = skrollr.init();
-	}
+        $(".menu").attr(("data-" + height), "background-color:rgba(192, 57, 43, 1); color: rgb(255, 255, 255);");
+        $(".menu").attr(("data-" + (height + 1)), "background-color:rgba(255, 255, 255, 0.9); color: rgb(150, 40, 27)");
+        var s = skrollr.init();
+    }
 
 
-	var first = $("div#first");
-	var runAnima = function() {
-  		first.delay(200).fadeTo(600, 0.3);
-  		first.next().delay(400).fadeTo(600, 0.3);
-  		first.next().next().delay(600).fadeTo(600, 0.3);
-  		first.next().next().next().delay(800).fadeTo(600, 0.3);
-  		first.delay(600).fadeTo(600, 1);
-  		first.next().delay(700).fadeTo(600, 1);
-  		first.next().next().delay(800).fadeTo(600, 1);
-  		first.next().next().next().delay(900).fadeTo(600, 1);
-  		if(!scrolled) {
-  			setTimeout(runAnima, 4000);
-  		}
-	}
+    var first = $("div#first");
+    var runAnima = function() {
+        first.delay(200).fadeTo(600, 0.3);
+        first.next().delay(400).fadeTo(600, 0.3);
+        first.next().next().delay(600).fadeTo(600, 0.3);
+        first.next().next().next().delay(800).fadeTo(600, 0.3);
+        first.delay(600).fadeTo(600, 1);
+        first.next().delay(700).fadeTo(600, 1);
+        first.next().next().delay(800).fadeTo(600, 1);
+        first.next().next().next().delay(900).fadeTo(600, 1);
+        setTimeout(runAnima, 4000);
+    }
 
-	init();
-	runAnima();
+    init();
+    runAnima();
 
     $(window).scroll(function() {
-        scrolled = true;
+        //scrolled = true;
     })
 
-  	$(".logo").click(function() {
-  		console.log("height is " + height);
-  	})
+    $(".logo").click(function() {
+        console.log("height is " + height);
+    })
 });

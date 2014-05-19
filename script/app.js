93c0a60c00918d55e4dd01681857f3f5fcf06680
Eric Lin <tinyjingle@gmail.com>
2014-05-18 23:52:43 -0400

    Coloring is hard

--- script/app.js
+++ script/app.js
@@ -21,8 +21,16 @@ $(document).ready(function(){
             $(".scroll-container").hide();
         }
         //menu scrolling setting
-        $(".menu").attr(("data-" + height), "background-color:rgba(127, 140, 141, 1); color: rgb(255, 255, 255);");
+        $(".menu").attr(("data-" + height), "background-color:rgba(255, 192, 0, 1); color: rgb(0, 0, 0);");
         $(".menu").attr(("data-" + (height + 1)), "background-color:rgba(255, 255, 255, 0.9); color: rgb(48, 48, 48)");
+        //Experience container
+        $(".experience-container-right").css("left", (width - 800) + "px");
+        var expList = $(".all-experience-container").find(".experience-container");
+        for (var i = 0; i < expList.length; i++) {
+            $(expList[i]).attr("data--50-bottom", "left:" + ($(expList[i]).css("left")));
+            $(expList[i]).attr("data--200-bottom", "left:" + (width - 800) / 2 + "px");
+        }
+        // $(".experience-container").attr("data--400-bottom", "left:" + ());
 
         var s = skrollr.init();
     }
